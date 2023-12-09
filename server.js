edcbff597571728417a6225a5e616a71c8bfb259
Nikolay <repyah84@gmail.com>
2023-12-09 14:29:55 +0200

    add video name

--- server.js
+++ server.js
@@ -13,19 +13,28 @@ app.listen(4000, () => {
 });
 
 app.get("/download", (req, res) => {
-  const URL = req.query.URL;
+  try {
+    const URL = req.query.URL;
+    const name = req.query.name;
 
-  res.header("Content-Disposition", 'attachment; filename="video.mp4"');
+    res.header("Content-Disposition", `attachment; filename="${name}.mp4"`);
 
-  ytdl(URL, {
-    format: "mp4",
-  }).pipe(res);
+    ytdl(URL, {
+      format: "mp4",
+    }).pipe(res);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 app.get("/info", (req, res) => {
-  const URL = req.query.URL;
+  try {
+    const URL = req.query.URL;
 
-  ytdl.getBasicInfo(URL).then((info) => {
-    res.json(info);
-  });
+    ytdl.getBasicInfo(URL).then((info) => {
+      res.json(info);
+    });
+  } catch (error) {
+    console.error(error);
+  }
 });
