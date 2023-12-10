65bd4032995af950bebe1169b64c261544b11791
Nikolay <repyah84@gmail.com>
2023-12-10 17:51:23 +0200

    add cookie

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ app.listen(1000, () => {
   console.log("Server Works !!! At port 1000");
 });
 
+const cs =
+  "YSC=hS9DrUTadOA; VISITOR_INFO1_LIVE=RGxQC-ibplE; PREF=tz=Europe.Kiev; SID=eAhvNe0gRjetK6L92QK2J-2O2luf4usNLL8NIvVKjZyXSbweIN5fD_atH4-SH_guheEqFA.; __Secure-1PSID=eAhvNe0gRjetK6L92QK2J-2O2luf4usNLL8NIvVKjZyXSbweGec02wkyjWWCyXTN-S68ZQ.; __Secure-3PSID=eAhvNe0gRjetK6L92QK2J-2O2luf4usNLL8NIvVKjZyXSbwe8HKL_o44bPV82Iygzs8uWQ.; HSID=A7H7el5piMaPPIjef; SSID=Atu2kBHXieb9pxhxQ; APISID=ip2EZwfIviaTCkUX/A9_J_lZy_exdvWQIU; SAPISID=Hh8QGo8Dd3xWHcfa/ALfBmog-TKTsVM92p; __Secure-1PAPISID=Hh8QGo8Dd3xWHcfa/ALfBmog-TKTsVM92p; __Secure-3PAPISID=Hh8QGo8Dd3xWHcfa/ALfBmog-TKTsVM92p; LOGIN_INFO=AFmmF2swRgIhAJFip-vvJGvktYFuusTNGSyVweb_WLeMaFzMwotT-o1kAiEA6R_0XXcq6W-JlA0H6Z-aaaaA2TRhdz9V-WqHtpPX9JQ:QUQ3MjNmd2tTWjlrRDB3amYzTkM0QkVmUkNZU0pzNW5uallwTmI0V0o5VzdaVlVrNWlrYjNFeG02M0ZEMHMwTjBMRzFINnVISU5OcWVIZ0dQeUZEQV9TVE54UGJWMVlWT3JwdW9DNmdiRmszZUlEVFFJNWRRY29iRmxpLW1ELTBYaV9YSWF2V3J6ZUZucE5LYUxTQXJMb2lRQkNsTUZEZTBjbXpvTFB3RFkwWXNic2FwYjJkM2VKdTdWb1BqUjl2b0dBLWdyRjdUajgtZnVNeGRmQnlmQ05ZNUwxRktGOVZzUQ==; VISITOR_PRIVACY_METADATA=CgJVQRIEGgAgNg%3D%3D; __Secure-1PSIDTS=sidts-CjEBPVxjSqZfwjmgivIdauPZWBeSGnbZC8h6-RXJcCoIwF_QJd1CaM3GePCNBKKewIlQEAA; __Secure-3PSIDTS=sidts-CjEBPVxjSqZfwjmgivIdauPZWBeSGnbZC8h6-RXJcCoIwF_QJd1CaM3GePCNBKKewIlQEAA; SIDCC=ACA-OxM4FD1mAVPTyzDflnvbl1vuIRJO0H8u21KNTag84AXDDLvMNSaw3TduT35Kv7NMo4xpyQ; __Secure-1PSIDCC=ACA-OxPegkdF6yxuuP1Msv3rrvcmQzU_ZK4hbIrdT9BKvhH64_-TQP02cwE-GQ4tcVympgkstf0; __Secure-3PSIDCC=ACA-OxPInloW6N7keErrZlU31d4MSDlsFKwMYIWfsT7OtgRV7uv93dUu6CQuVuUDbZSjSdoRPfc";
+
 app.get("/download", (req, res) => {
   try {
     const URL = req.query.URL;
@@ -21,12 +24,15 @@ app.get("/download", (req, res) => {
 
     res.writeHead(200, {
       "Content-Disposition": contentDisposition(`${name}.mp4`),
-      "x-youtube-identity-token":
-        "QUFFLUhqblVveUdMNGhRVVByd2pabzg2T1FUeUM0Z2pmUXw=",
     });
 
     ytdl(URL, {
       format: "mp4",
+      requestOptions: {
+        Cookie: cs,
+        "X-Youtube-Identity-Token":
+          "QUFFLUhqblVveUdMNGhRVVByd2pabzg2T1FUeUM0Z2pmUXw=",
+      },
     }).pipe(res);
   } catch (error) {
     console.error(error);
@@ -44,7 +50,8 @@ app.get("/info", (req, res) => {
     ytdl
       .getBasicInfo(URL, {
         requestOptions: {
-          "x-youtube-identity-token":
+          Cookie: cs,
+          "X-Youtube-Identity-Token":
             "QUFFLUhqblVveUdMNGhRVVByd2pabzg2T1FUeUM0Z2pmUXw=",
         },
       })
