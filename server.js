7a4981bc920eb6719f42a3e0be9353b69aac2080
Nikolay <repyah84@gmail.com>
2023-12-19 20:20:50 +0200

    refactor api

--- server.js
+++ server.js
@@ -23,6 +23,29 @@ app.get("/info", async ({ query: { id } }, res) => {
   }
 });
 
+app.get("/info-with-quality", async ({ query: { id } }, res) => {
+  try {
+    const info = await ytdl.getInfo(id);
+
+    const listOfVideoQuality = info.formats.filter(
+      (item) => item.hasAudio && item.hasVideo
+    );
+
+    const listOfAudioQuality = info.formats
+      .filter((item) => item.hasAudio && !item.hasVideo)
+      .map((item) => ({
+        ...item,
+        qualityLabel: item.audioBitrate,
+      }));
+
+    res.json({ ...info, listOfVideoQuality, listOfAudioQuality });
+  } catch (error) {
+    console.error(error);
+
+    res.json({ error });
+  }
+});
+
 app.get("/download", ({ query: { id, name } }, res) => {
   try {
     res.writeHead(200, {
@@ -36,3 +59,22 @@ app.get("/download", ({ query: { id, name } }, res) => {
     res.json({ error });
   }
 });
+
+app.get(
+  "/download-with-quality",
+  async ({ query: { id, name, itag, qualityLabel, container }, res }) => {
+    try {
+      res.writeHead(200, {
+        "Content-Disposition": contentDisposition(
+          `${name}_${qualityLabel}.${container}`
+        ),
+      });
+
+      ytdl(id, { filter: (format) => (format.itag = itag) }).pipe(res);
+    } catch (error) {
+      console.error(error);
+
+      res.json({ error });
+    }
+  }
+);
